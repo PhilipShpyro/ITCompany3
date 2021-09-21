acfde9d9f050e0b5bfbdb3a6edea4524ffc0c7cf
PhilipShpyro <55670267+PhilipShpyro@users.noreply.github.com>
2021-09-21 23:10:41 +0300

    Starting rewriting project. Everything will be removed and renewed.

--- src/com/PhilipShpyro/PHPDeveloper.java
+++ src/com/PhilipShpyro/PHPDeveloper.java
@@ -8,7 +8,7 @@ public class PHPDeveloper extends Developer {
 
     @Override
     public TechnologyType getTechnologyType() {
-        return TechnologyType.JAVA;
+        return TechnologyType.PHP;
     }
 
     @Override
